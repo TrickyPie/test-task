5c92a2f6559444b4723cc35d7a3c9e0b2c1172ab
Trickypie <klimova.pr@gmail.com>
2023-10-31 22:45:39 +0300

    feat: update breadcrumbs component

--- app/advertisers/orders/order/application/page.tsx
+++ app/advertisers/orders/order/application/page.tsx
@@ -1,25 +1,38 @@
-import { Center, Box, Container, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Link, Heading, Select, Button, SimpleGrid, Image } from '@chakra-ui/react';
 import Breadcrumbs from '../../../../components/Breadcrumbs';
+import NextLink from 'next/link';
 
 export default function Application(): JSX.Element {
   return (
-    <Center>
-      <Container>
-        <Flex flexDirection="column" gap={4}>
-          <Box>
-            <Breadcrumbs />
-          </Box>
-          <Box>
-            <Text>Box 2</Text>
-          </Box>
-          <Box>
-            <Text>Box 3</Text>
-          </Box>
-          <Box>
-            <Text>Box 4</Text>
-          </Box>
-        </Flex>
-      </Container>
-    </Center>
+    <Flex p={10} alignItems="center" justifyContent="center">
+      <Flex flexDirection="column" gap={4} height="100%">
+        <Box>
+          <Breadcrumbs />
+        </Box>
+        <Box>
+          <Flex gap={4}>
+            <Link as={NextLink} href="/advertisers/orders/order/">
+              <Image boxSize={6} objectFit="cover" src="/chevron-left.svg" alt="Обратно к заказу" />
+            </Link>
+            <Heading as="h4" size="md">
+              Заявки по заказу #0b6f729f-abdf-4919-92f5-31875d790179
+            </Heading>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex gap={4}>
+            <Select variant="outline" placeholder="Выберите статус">
+              <option value="option1">Одобрено</option>
+              <option value="option2">Отклонено</option>
+              <option value="option3">Ожидает решения</option>
+            </Select>
+            <Button colorScheme="gray">Выбрать к одобрению/отклонению</Button>
+          </Flex>
+        </Box>
+        <Box>
+          <SimpleGrid></SimpleGrid>
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -4,29 +4,40 @@ import NextLink from 'next/link';
 export default function Breadcrumbs(): JSX.Element {
   return (
     <Breadcrumb separator=">">
-      {/* <Icon color="gray.300" name="chevron-right" /> */}
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/'}>
+        <BreadcrumbLink as={NextLink} href={'/'} _hover={{ color: 'blue.500' }}>
           Главная
         </BreadcrumbLink>
       </BreadcrumbItem>
+
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/advertisers'}>
+        <BreadcrumbLink as={NextLink} href={'/advertisers'} _hover={{ color: 'blue.500' }}>
           Рекламодатели
         </BreadcrumbLink>
       </BreadcrumbItem>
+
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/advertisers/orders'}>
+        <BreadcrumbLink as={NextLink} href={'/advertisers/orders'} _hover={{ color: 'blue.500' }}>
           Заказы
         </BreadcrumbLink>
       </BreadcrumbItem>
+
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/advertisers/orders/order'}>
+        <BreadcrumbLink
+          as={NextLink}
+          href={'/advertisers/orders/order'}
+          _hover={{ color: 'blue.500' }}
+        >
           Заказ
         </BreadcrumbLink>
       </BreadcrumbItem>
+
       <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink as={NextLink} href={'/advertisers/orders/order/application'}>
+        <BreadcrumbLink
+          as={NextLink}
+          href={'/advertisers/orders/order/application'}
+          _hover={{ color: 'blue.500' }}
+        >
           Заявки
         </BreadcrumbLink>
       </BreadcrumbItem>
