660643f619350d098ac49fb50257e9f8f5607572
Trickypie <klimova.pr@gmail.com>
2023-11-02 01:27:00 +0300

    refactor: change svg icons to components

---
+++ public/chevron-down.js
@@ -0,0 +1,23 @@
+import { Icon } from '@chakra-ui/react';
+import React from 'react';
+
+const ChevronDown = (props) => (
+  <Icon viewBox="0 0 330 330" {...props}>
+    <svg
+      fill="#000000"
+      height="330px"
+      width="330px"
+      version="1.1"
+      id="Layer_1"
+      viewBox="0 0 330 330"
+    >
+      <path
+        id="XMLID_102_"
+        d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+      ></path>
+    </svg>
+    ;
+  </Icon>
+);
+
+export default ChevronDown;
---
+++ public/chevron-right.js
@@ -0,0 +1,12 @@
+import { Icon } from '@chakra-ui/react';
+import React from 'react';
+
+const ChevronRight = (props) => (
+  <Icon viewBox="0 0 24 24" {...props}>
+    <svg fill="#404040" width="24px" height="24px" viewBox="0 0 24 24">
+      <path d="M9.343 18.657a1 1 0 0 1-.707-1.707l4.95-4.95-4.95-4.95a1 1 0 0 1 1.414-1.414l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-.707.293z" />
+    </svg>
+  </Icon>
+);
+
+export default ChevronRight;
