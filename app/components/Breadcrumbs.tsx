caa9af0edc9519f8cd010d39947d85a7e647fa38
Trickypie <klimova.pr@gmail.com>
2023-11-02 14:56:57 +0300

    refator: update breadcrumbs and pagination layout

--- app/components/Breadcrumbs.tsx
+++ app/components/Breadcrumbs.tsx
@@ -1,23 +1,35 @@
-import { Breadcrumb, BreadcrumbLink, BreadcrumbItem } from '@chakra-ui/react';
+import { Breadcrumb, BreadcrumbLink, BreadcrumbItem, Box } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import ChevronRight from '../../public/chevron-right';
 
 export default function Breadcrumbs(): JSX.Element {
   return (
-    <Breadcrumb separator=">">
+    <Breadcrumb
+      separator={
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <ChevronRight />
+        </Box>
+      }
+    >
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/'} _hover={{ color: 'blue.500' }}>
+        <BreadcrumbLink as={NextLink} href={'/'} _hover={{ color: 'blue.500' }} p="2">
           Главная
         </BreadcrumbLink>
       </BreadcrumbItem>
 
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/advertisers'} _hover={{ color: 'blue.500' }}>
+        <BreadcrumbLink as={NextLink} href={'/advertisers'} _hover={{ color: 'blue.500' }} p="2">
           Рекламодатели
         </BreadcrumbLink>
       </BreadcrumbItem>
 
       <BreadcrumbItem>
-        <BreadcrumbLink as={NextLink} href={'/advertisers/orders'} _hover={{ color: 'blue.500' }}>
+        <BreadcrumbLink
+          as={NextLink}
+          href={'/advertisers/orders'}
+          _hover={{ color: 'blue.500' }}
+          p="2"
+        >
           Заказы
         </BreadcrumbLink>
       </BreadcrumbItem>
@@ -27,6 +39,7 @@ export default function Breadcrumbs(): JSX.Element {
           as={NextLink}
           href={'/advertisers/orders/order'}
           _hover={{ color: 'blue.500' }}
+          p="2"
         >
           Заказ
         </BreadcrumbLink>
@@ -37,6 +50,7 @@ export default function Breadcrumbs(): JSX.Element {
           as={NextLink}
           href={'/advertisers/orders/order/application'}
           _hover={{ color: 'blue.500' }}
+          p="2"
         >
           Заявки
         </BreadcrumbLink>
--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,13 +1,24 @@
-import { Flex, Text, Image, Spacer } from '@chakra-ui/react';
+import { Flex, Text, Image, Spacer, Box } from '@chakra-ui/react';
 
 export default function Pagination(): JSX.Element {
   return (
-    <Flex>
+    <Flex alignItems="center">
       <Text>Страница 1 из 1</Text>
       <Spacer></Spacer>
-      <Flex>
+      <Flex alignItems="center" gap="2">
         <Image boxSize="5" objectFit="cover" src="/chevron-left.svg" alt="На предыдущую страницу" />
-        <Text>1</Text>
+        <Box
+          bg="gray.200"
+          p="3"
+          borderRadius="5px"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text w="5" h="5" textAlign="center">
+            1
+          </Text>
+        </Box>
         <Image boxSize="5" objectFit="cover" src="/chevron-right.svg" alt="На следующую страницу" />
       </Flex>
     </Flex>
