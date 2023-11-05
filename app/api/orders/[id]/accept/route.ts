75465d2a4792e5d85664da8e6a1ee06758417df6
Trickypie <klimova.pr@gmail.com>
2023-11-05 18:07:47 +0300

    feat: create put request

---
+++ app/api/orders/[id]/accept/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { ordersList } from '../../../../faker';
+
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+  const currentId = params.id;
+  const order = ordersList.find((el) => el.id === +currentId);
+
+  if (order) {
+    order.accepted = true;
+    return NextResponse.json(order, { status: 200 });
+  } else {
+    return NextResponse.json(
+      { message: `Sorry, oder with id ${params.id} not found` },
+      { status: 404 }
+    );
+  }
+}
