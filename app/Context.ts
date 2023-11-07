5daea6d2b1e9b433df80b7326856bff54c03c0ad
Trickypie <klimova.pr@gmail.com>
2023-11-08 00:56:14 +0300

    feat: add context file

--- app/Context.ts
+++ app/Context.ts
@@ -3,11 +3,15 @@ import { Dispatch, SetStateAction, createContext } from 'react';
 type buttonStatusContextType = {
   buttonStatus: boolean;
   setButtonStatus: Dispatch<SetStateAction<boolean>>;
+  counter: number;
+  setCounter: Dispatch<SetStateAction<number>>;
 };
 
 const buttonStatusContext = createContext<buttonStatusContextType>({
   buttonStatus: false,
   setButtonStatus: () => {},
+  counter: 0,
+  setCounter: () => {},
 });
 
 export default buttonStatusContext;
