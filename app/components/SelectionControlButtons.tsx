291c42a1e14474a0c1923101d7aab7aaf7af3f54
Trickypie <klimova.pr@gmail.com>
2023-11-09 19:21:26 +0300

    refactor: remove unnecessary useeffect and usestate, the value is taken directly from the context

--- app/components/SelectionControlButtons.tsx
+++ app/components/SelectionControlButtons.tsx
@@ -1,24 +1,19 @@
 import { Button, Flex } from '@chakra-ui/react';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import context from '../Context';
 
 export default function SelectionControlButtons(): JSX.Element {
-  const [count, setCount] = useState(0);
   const { counter } = useContext(context);
-
-  useEffect(() => {
-    setCount(counter);
-  }, [counter]);
 
   return (
     <Flex alignSelf="end" gap="2" marginTop="8">
       <Button>Сбросить</Button>
       <Button
-        backgroundColor={count === 0 ? '#aca5fc' : '#422AFB'}
+        backgroundColor={counter === 0 ? '#aca5fc' : '#422AFB'}
         color="#ffffff"
         _hover={{ color: '#ffffff' }}
       >
-        Выбрано заявок: {count}
+        Выбрано заявок: {counter}
       </Button>
     </Flex>
   );
